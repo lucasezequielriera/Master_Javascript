642125f6b8441513a52cef2f9d6a1cc2c59e49c7
Lucas Riera <lucasezequielriera@hotmail.com>
2021-04-27 19:56:27 -0300

    AJAX con GET y POST

---
+++ js/ajax.js
@@ -0,0 +1,20 @@
+'use strict'
+
+$(document).ready(function() {
+    // // Método LOAD //
+    // $("#datos").load("https://reqres.in/");
+
+    // // Método GET //
+    // $.get("https://reqres.in/api/users", {page: 2}, function(response) {
+    //     response.data.forEach((element, index) => {
+    //         $("#datos").append(`<p> ${element.first_name} ${element.last_name} </p>`);
+    //     });
+    // });
+
+    // Método POST //
+    const usuario = {
+        nombre: "Lucas Riera",
+        web: "lucasriera.com"
+    };
+    
+});
